ac0b79eadc0f1d3c7d0f87d70097b49e15178d16
kilesh-mhzn <kileshmhrzn@gmail.com>
2023-08-07 22:09:52 +0545

    feat: create tabs component to switch between pokemon generations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import "./App.css";
+import { useState } from "react";
+import GenerationSelector from "./components/generationSelector";
 
 function App() {
+  const tabs = [
+    { id: 1, label: "I" },
+    { id: 2, label: "II" },
+    { id: 3, label: "III" },
+    { id: 4, label: "IV" },
+    { id: 5, label: "V" },
+    { id: 6, label: "VI" },
+    { id: 7, label: "VII" },
+    { id: 8, label: "VIII" },
+  ];
+  const [currentTab, setCurrentTab] = useState<number>(0);
+
+  const handleTabChange = (activeTab: number) => {
+    setCurrentTab(activeTab);
+  };
   return (
     <>
-      <div className="text-red-500 text-3xl">Pokedex App</div>
+      <div className="background-container"></div>
+      <div className="container mx-auto">
+        <div className="text-center">Pokédex App</div>
+        <GenerationSelector tabs={tabs} onTabChange={handleTabChange} />
+        <div>Current Tab: {currentTab + 1}</div>
+      </div>
     </>
   );
 }
