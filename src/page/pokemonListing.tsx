33ff943c16c1a4ffd52edeb02c3e4375945d18d4
kilesh-mhzn <kileshmhrzn@gmail.com>
2023-08-11 10:04:25 +0545

    setup github pages

--- src/page/pokemonListing.tsx
+++ src/page/pokemonListing.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 import GenerationSelector from "../components/GenerationSelector";
 import PokemonCards from "../components/PokemonCards";
 import Loader from "../components/loader";
