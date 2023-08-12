cad252fe63b787c8895197835e7d1145f857f44c
kilesh-mhzn <kileshmhrzn@gmail.com>
2023-08-12 23:41:53 +0545

    hotfix

--- src/components/RamroToast.tsx
+++ src/components/RamroToast.tsx
@@ -12,7 +12,7 @@ const Toast: React.FC<ToastProps> = ({ message, onClose }) => {
     const timeout = setTimeout(() => {
       setVisible(false);
       onClose();
-    }, 3000); // Duration in milliseconds
+    }, 3000);
 
     return () => {
       clearTimeout(timeout);
@@ -21,10 +21,9 @@ const Toast: React.FC<ToastProps> = ({ message, onClose }) => {
 
   return (
     <div
-      className={`${visible ? "animate-slideIn" : "animate-slideOut"} 
-      fixed bottom-4 left-4 bg-slate-50 text-slate-800 dark:bg-gray-800 dark:text-white-100 
-      px-4 py-2 rounded-md shadow-md 
-      font-semibold`}
+      className={`${visible ? "animate-slideOut" : "animate-slideIn"} 
+      fixed top-10 right-1/2 translate-x-1/2 bg-slate-50 text-slate-800 dark:bg-gray-800 dark:text-white-100 
+      px-4 py-2 rounded-md shadow-md font-semibold z-[9999]`}
     >
       {message}
     </div>
