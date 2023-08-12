8a12778d17070f35a75930ffb67559d9e3e8969b
kilesh-mhzn <kileshmhrzn@gmail.com>
2023-08-12 14:43:56 +0545

    hot-fix: logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,7 @@ export const Logo = () => {
     <div>
       <div className="flex justify-center items-baseline tracking-wide text-6xl font-extrabold my-6">
         P
-        <img
-          className="mr-[0.025em]"
-          width={36}
-          src="public\logo.svg"
-          alt="o"
-        />
+        <img className="mr-[0.025em]" width={36} src="/logo.svg" alt="o" />
         <span className="underline underline-offset-8 decoration-red-400">
           kéd
         </span>
