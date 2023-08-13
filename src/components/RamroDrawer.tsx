9dda0edc4e53ef367ea996f36c78a6b9849e8c4e
kilesh-mhzn <kileshmhrzn@gmail.com>
2023-08-13 17:02:47 +0545

    refactor: drawer redesign

--- src/components/RamroDrawer.tsx
+++ src/components/RamroDrawer.tsx
@@ -9,7 +9,7 @@ interface DrawerProps {
 
 const RamroDrawer: React.FC<DrawerProps> = ({ isOpen, onClose, children }) => {
   const drawerClassName = `z-[90] md:w-max fixed top-0 right-0 w-[90%]
-   h-full bg-white-100 dark:bg-slate-800 shadow-xl transform transition-transform 
+   h-full bg-slate-50 dark:bg-slate-800 shadow-xl transform transition-transform 
    ${isOpen ? "translate-x-0" : "translate-x-full"}`;
 
   useEffect(() => {
@@ -23,12 +23,18 @@ const RamroDrawer: React.FC<DrawerProps> = ({ isOpen, onClose, children }) => {
   return (
     <div className={drawerClassName}>
       <div className="">
-        <div
-          onClick={onClose}
-          className="absolute top-2 right-4 text-gray-500 hover:text-gray-700 cursor-pointer z-50"
-        >
-          <IconClose size="1.5em" color="#000" />
-        </div>
+        {isOpen && (
+          <div
+            onClick={onClose}
+            className={`absolute  left-[-30px] hover:text-white-100 cursor-pointer z-50
+          bg-slate-50 dark:bg-slate-800 p-3 rounded-full shadow-xl`}
+          >
+            <IconClose
+              size="1.5em"
+              className="text-slate-800 dark:text-slate-50"
+            />
+          </div>
+        )}
         {children}
       </div>
     </div>
